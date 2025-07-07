062cd0a4301576e8e2992a44a6acf81a702268d7
Shahid-Chawdhary <shahidapp@itempire.com>
2025-07-07 18:49:20 +0500

    vector icons commit

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import AppNavigator from './src/navigation/AppNavigator.tsx';
+import AppNavigator from './src/navigation/AppNavigator';
 import { MenuProvider } from 'react-native-popup-menu';
 import Toast from 'react-native-toast-message';
 
