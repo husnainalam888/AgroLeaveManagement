cf65f6acc327710acdfea0eb189ddefbcc25617d
Shahid-Chawdhary <shahidapp@itempire.com>
2025-07-11 10:32:36 +0500

    fixed safe area view

--- App.tsx
+++ App.tsx
@@ -3,16 +3,19 @@ import { NavigationContainer } from '@react-navigation/native';
 import AppNavigator from './src/navigation/AppNavigator';
 import { MenuProvider } from 'react-native-popup-menu';
 import Toast from 'react-native-toast-message';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { StatusBar } from 'react-native';
 
 export default function App() {
   return (
-    <>
+    <SafeAreaView style={{ flex: 1, backgroundColor: 'transparent' }}>
+      <StatusBar barStyle="dark-content" />
       <MenuProvider>
         <NavigationContainer>
           <AppNavigator />
         </NavigationContainer>
       </MenuProvider>
       <Toast />
-    </>
+    </SafeAreaView>
   );
 }
